8583f514a786e7ff99637efb98732424446ae6f2
final <julyfinal@outlook.com>
2021-03-10 18:18:13 +0800

    add nlp test

---
+++ NLP/conlptest.py
@@ -0,0 +1,13 @@
+from cocoNLP.extractor import extractor
+ex = extractor()
+text = '急寻特朗普，男孩，于2018年11月27号11时在陕西省安康市汉滨区走失。丢失发型短发，...如有线索，请迅速与警方联系：18100065143，132-6156-2938，baizhantang@sina.com.cn 和yangyangfuture at gmail dot com'
+locations = ex.extract_locations(text)
+print(locations)
+
+
+from pyhanlp import *
+
+print(HanLP.segment('你好，欢迎在Python中调用HanLP的API'))
+PerceptronLexicalAnalyzer = JClass('com.hankcs.hanlp.model.perceptron.PerceptronLexicalAnalyzer')
+analyzer = PerceptronLexicalAnalyzer()
+print(analyzer.analyze("上海华安工业（集团）公司董事长谭旭光和秘书胡花蕊来到美国纽约现代艺术博物馆参观"))
